195e97d887fae9d0f9b8dada6aa1f5a11f7de143
Pavlo Galkin <galkinletter@ukr.net>
2019-11-11 12:20:49 +0200

    Fix: fix matrix key by asm

--- Lesson4_Matrix_Key/codevision/main.c
+++ Lesson4_Matrix_Key/codevision/main.c
@@ -7,49 +7,46 @@ void main ()
 {
 DDRD = 0xFF;
 PORTD = 0b01010101; 
-DDRE = 0b11110000;
-PORTE.6 = 0;
-PORTE.7 = 0;
-//asm program 
+//DDRE = 0b11110000;
+//PORTE.6 = 0;
+//PORTE.7 = 0;
+
+////asm program 
 #asm
-;ldi r19,0xFF
-;out $11,r19
-ldi r19,0b11010000
-ldi r26,0b11110000
-ldi r27,0b11111111
-ldi r17,0b00001111
-ldi r20,0b00001111
-ldi r16,0b00001111
-ldi r18,0b00000000
-////////////
-ldi r21, 0b1111110
-ldi r22, 0b1111101
-ldi r23, 0b1111011
-ldi r24, 0b1110111
-ldi r25, 0b1101111
-////////////
-out $11,r27
-;out $61,r18
-out $12, r21
-nop
-;out $02, r26
-;out $03, r19
-nop
-nop
+; init
+ldi r19, 0b11110000
+ldi r20, 0b11100000
+ldi r21, 0b11010000
+ldi r22, 0b00001111
+; set line E0-E3 to in and set E4-E7 to out  
+out $02,r19
+;start loop
 lb1:
-out $12, r22 
-in r16, $00
-cp r16, r17
+out $03,r20 ; set 0 to PORTE.7
 nop
-cp r16, r17
-breq lb1
+in r17,$01 ; read PINE and write value to r17 - save data from 
+out $03,r21 ; set 0 to PORTE.6
 nop
-out $12, r20
+in r18, $01 ; read PINE and write value to r18  
+and r17, r22
+and r18,r22
+cp r17, r22
+brne lb2 
+cp r18, r22
+brne lb2	
+jmp lb1
+lb2:				; мітка виходу з процедури
+out $12, r22
 #endasm
 
 //C Program 
+delay_ms(2000);
+PORTD = 0xF0;
+delay_ms(2000);
+PORTD = 0x0F;
+delay_ms(2000); 
 
- while (1){ 
+while (1){ 
     
    if (PINE.1 == 0){
    PORTD = 0b11111110; 
